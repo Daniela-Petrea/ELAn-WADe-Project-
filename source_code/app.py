362e98080104a9caa0b5b34876a316d8cc8262a1
Puscasu Bogdan <bogdan_puscasu2001@yahoo.com>
2025-01-03 18:25:31 +0200

    sorted results alphabetically

--- source_code/app.py
+++ source_code/app.py
@@ -14,6 +14,7 @@ def home():
     SELECT ?language WHERE {
         ?language rdf:type esolang:EsotericLanguage .
     }
+    ORDER BY ?language
     """
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
